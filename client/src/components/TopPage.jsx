917a941eb7c42dc543ec4256321b1edbeba6d3a1
MIYAKOES <miyakokkoa385@gmail.com>
2025-08-03 17:05:04 +0900

    Add TopPage component

---
+++ client/src/components/TopPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TopPage() {
+  return (
+    <div>
+      <h2>Mix Drink SNS へようこそ！</h2>
+      <p>ここでは、お気に入りのドリンクを投稿したり、他の人の投稿を閲覧したりできます。</p>
+      <p>メニューから各機能をお試しください。</p>
+    </div>
+  );
+}
